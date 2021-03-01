c0a0332629f84a2771d5818efc999f7af89401ee
gegorov <gegorov@yahoo.com>
2021-03-02 01:39:14 +0300

    add static content serving

--- src/routes/admin.js
+++ src/routes/admin.js
@@ -1,14 +1,12 @@
 const express = require('express');
+const path = require('path');
+
+const rootDir = require('../util/path');
 
 const router = express.Router();
 
 router.get('/add-product', (req, res) => {
-  res.send(`
-  <form action="/admin/product" method="POST">
-    <input type="text" name="title">
-    <button type="submit">Add product</button>
-  </form>
-  `);
+  res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
 });
 
 router.post('/product', (req, res) => {
