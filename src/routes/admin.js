8e1c355c2cc5481ed5845a61694e1f9f34002dd6
gegorov <gegorov@yahoo.com>
2021-03-02 01:54:19 +0300

    share data between routes

--- src/routes/admin.js
+++ src/routes/admin.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path');
 
+const products = [];
 const rootDir = require('../util/path');
 
 const router = express.Router();
@@ -9,9 +10,11 @@ router.get('/add-product', (req, res) => {
   res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
 });
 
-router.post('/product', (req, res) => {
+router.post('/add-product', (req, res) => {
   console.log(req.body);
+  products.push({ title: req.body.title });
   res.redirect('/');
 });
 
-module.exports = router;
+module.exports.routes = router;
+module.exports.products = products;
